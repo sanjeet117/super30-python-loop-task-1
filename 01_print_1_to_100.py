0b894d35a2896af6590b71bf5ca3045b12882c93
sanjeet117 <sanjeethpekumar@gmail.com>
2026-09-05 00:21:22 +0530

    Update 01_print_1_to_100.py

--- 01_print_1_to_100.py
+++ 01_print_1_to_100.py
@@ -1,5 +1,5 @@
 # Write a program to print numbers from 1 to 100 using a for loop.
-for i in range(1,100):
+for i in range(1,101):
   print(i,end = ' ')
 
 # we have used range function to take range from 1 to 100 and using for loop printed each and used end
